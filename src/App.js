8a6d49cb588e7666bd54a4d631f6b0e10c746e68
Ahmed Ali <ahmedawan43011@gmail.com>
2022-04-06 16:50:37 +0500

    passing props through  intended component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 
 import './App.css';
-let guest = 0;
+
 
 function Cup() {
-  // Bad: changing a preexisting variable!
-  guest = guest + 1;
+  
   return <h2>Tea cup for guest #{guest}</h2>;
 }
 function App() {
   return (
     <>
-      <Cup />
-      <Cup />
-      <Cup />
+      <Cup guest={1}/>
+      <Cup guest={2}/>
+      <Cup guest={3}/>
     </>
   );
 }
